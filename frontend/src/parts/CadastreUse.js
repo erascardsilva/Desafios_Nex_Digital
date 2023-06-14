3e7e2f041df1d33e951b52a3068c77a3b86625a7
Erasmo Cardoso <erasmo@vmi-informatica.com.br>
2023-06-14 01:32:19 -0300

    update

--- frontend/src/parts/CadastreUse.js
+++ frontend/src/parts/CadastreUse.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './FormC.css'; 
 
 const CadastreUser = () => {
   const [name, setName] = useState('');
@@ -13,7 +14,8 @@ const CadastreUser = () => {
     e.preventDefault();
     setSubmitting(true);
 
-    axios.post('http://localhost:3000/signup', { name, email, password })
+    axios
+      .post('http://localhost:3000/signup', { name, email, password })
       .then((response) => {
         setSubmitting(false);
         setSubmitted(true);
@@ -29,40 +31,43 @@ const CadastreUser = () => {
   };
 
   return (
-    <div>
+    <div className="cadastre-container">
       <h2>Cadastro de Usuário</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
+      <form className="cadastre-form" onSubmit={handleSubmit}>
+        <div className="form-group">
           <label htmlFor="name">Nome Completo:</label>
           <input
             type="text"
             id="name"
+            className="input-field"
             value={name}
             onChange={(e) => setName(e.target.value)}
             required
           />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="email">E-mail:</label>
           <input
             type="email"
             id="email"
+            className="input-field"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="password">Senha:</label>
           <input
             type="password"
             id="password"
+            className="input-field"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
         </div>
-        <button type="submit" disabled={submitting}>
+        <button type="submit" disabled={submitting} className="submit-button">
           {submitting ? 'Enviando...' : 'Cadastrar'}
         </button>
         {submitted && <p>Cadastro enviado com sucesso!</p>}
--- frontend/src/parts/LoginUser.js
+++ frontend/src/parts/LoginUser.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './Form.css';
 
 const LoginUser = () => {
   const [name, setName] = useState('');
@@ -13,7 +14,8 @@ const LoginUser = () => {
     e.preventDefault();
     setSubmitting(true);
 
-    axios.post('http://localhost:3000/signup', { name, email, password })
+    axios
+      .post('http://localhost:3000/signup', { name, email, password })
       .then((response) => {
         setSubmitting(false);
         setSubmitted(true);
@@ -29,41 +31,44 @@ const LoginUser = () => {
   };
 
   return (
-    <div>
+    <div className="login-container">
       <h2>Login de Usuário</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="form-group">
           <label htmlFor="name">Nome Completo:</label>
           <input
             type="text"
             id="name"
+            className="form-control"
             value={name}
             onChange={(e) => setName(e.target.value)}
             required
           />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="email">E-mail:</label>
           <input
             type="email"
             id="email"
+            className="form-control"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="password">Senha:</label>
           <input
             type="password"
             id="password"
+            className="form-control"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
         </div>
-        <button type="submit" disabled={submitting}>
-          {submitting ? 'Enviando...' : 'Cadastrar'}
+        <button type="submit" disabled={submitting} className="btn">
+          {submitting ? 'Enviando...' : 'Logar'}
         </button>
         {submitted && <p>Logando !</p>}
         {error && <p>{error}</p>}
